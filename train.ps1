cd4f052ceefad6f7360a38588490f11395784dbb
Tobias Fuchs <info@tobiasfuchs.de>
2020-11-30 00:56:25 +0100

    Add train script for deep q agent

--- train.ps1
+++ train.ps1
@@ -1,3 +1,4 @@
 $env:PYTHONPATH="."
 # python ./egd/simulation.py --player0 QLearningAgent --player1 QLearningAgent --player2 QLearningAgent --player3 QLearningAgent --games 25000 --verbose 0 --loadmodel 0 --savemodel 1 --inference 0
-python ./egd/simulation.py --player0 QLearningAgent --player1 Simple --player2 Random --player3 Simple --games 100000 --verbose 0 --loadmodel 0 --savemodel 1 --inference 0
+# python ./egd/simulation.py --player0 QLearningAgent --player1 Simple --player2 Random --player3 Simple --games 100000 --verbose 0 --loadmodel 0 --savemodel 1 --inference 0
+python ./egd/simulation.py --player0 DeepQAgent --player1 Simple --player2 Random --player3 Simple --games 100000 --verbose 0 --loadmodel 0 --savemodel 1 --inference 0
