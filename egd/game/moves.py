17f34e24101aab9a006e1aa8ac4523fa5fe55a45
Tobias Fuchs <info@tobiasfuchs.de>
2020-12-04 19:55:01 +0100

    Refactor next moves

--- egd/game/moves.py
+++ egd/game/moves.py
@@ -4,14 +4,55 @@ from egd.game.cards import NUM_CARD_VALUES, JOKER
 from egd.game.state import has_finished, get_cards_array
 
 
+def only_passing_possible(hand, board):
+    """
+        Faster than checking len(possible_next_moves(...)) == 1.
+    """
+
+    # Finished players have to pass
+    if has_finished(hand):
+        return True
+
+    # No passing allowed when board empty
+    if np.all(board == 0):
+        return False
+
+    # Else iterate possible actions
+    card_type_in_board = np.argmax(board)
+    num_cards_in_board = board[card_type_in_board] \
+        if card_type_in_board == JOKER \
+        else board[card_type_in_board] + board[JOKER]
+
+    for card_type_in_hand in range(NUM_CARD_VALUES - 1, -1, -1):
+        # You can play clean
+        if card_type_in_hand < card_type_in_board and \
+                hand[card_type_in_hand] >= num_cards_in_board:
+            return False
+
+        # Or you can play dirty (with Joker(s))
+        if card_type_in_hand != JOKER and hand[JOKER] > 0 \
+                and card_type_in_hand < card_type_in_board \
+                and num_cards_in_board >= 2 and hand[card_type_in_hand] > 0 \
+                and hand[card_type_in_hand] + hand[JOKER] >= num_cards_in_board:
+            # Use one joker
+            if hand[card_type_in_hand] + 1 >= num_cards_in_board:
+                return False
+
+            # Use two jokers
+            if hand[JOKER] == 2 and num_cards_in_board >= 3:
+                return False
+
+    # No possible actions available
+    return True
+
+
 def possible_next_moves(hand, board):
     """
-        Returns possible next moves as a list of tuples (new hand, new board)
+        Returns possible next moves as a list of actions
     """
 
     # You can always pass if it is not the initial move
-    possible_hands = np.reshape(hand, (1, NUM_CARD_VALUES))
-    possible_boards = np.reshape(board, (1, NUM_CARD_VALUES))
+    possible_actions = [np.zeros((1, NUM_CARD_VALUES), dtype=np.int8)]
 
     # If board empty, moves do only depend on hand
     if np.all(board == 0):
@@ -20,18 +61,15 @@ def possible_next_moves(hand, board):
                 if card_type != JOKER:
                     for num_jokers in range(hand[JOKER] + 1):
                         # Form new board out of jokers and cards
-                        new_board = get_cards_array(card_type, num_cards) + \
-                            get_cards_array(JOKER, num_jokers)
-                        new_hand = hand - new_board
-                        possible_hands = np.vstack([possible_hands, new_hand])
-                        possible_boards = np.vstack(
-                            [possible_boards, new_board])
+                        possible_actions.append(
+                            get_cards_array(card_type, num_cards)
+                            + get_cards_array(JOKER, num_jokers)
+                        )
                 else:
                     # Form new board out of only jokers
-                    new_board = get_cards_array(card_type, num_cards)
-                    new_hand = hand - new_board
-                    possible_hands = np.vstack([possible_hands, new_hand])
-                    possible_boards = np.vstack([possible_boards, new_board])
+                    possible_actions.append(
+                        get_cards_array(card_type, num_cards)
+                    )
 
     # Move has to match current board
     else:
@@ -45,11 +83,9 @@ def possible_next_moves(hand, board):
                 # You can play clean
                 if card_type_in_hand < card_type_in_board and \
                         hand[card_type_in_hand] >= num_cards_in_board:
-                    new_board = get_cards_array(
-                        card_type_in_hand, num_cards_in_board)
-                    new_hand = hand - new_board
-                    possible_hands = np.vstack([possible_hands, new_hand])
-                    possible_boards = np.vstack([possible_boards, new_board])
+                    possible_actions.append(
+                        get_cards_array(card_type_in_hand, num_cards_in_board)
+                    )
 
                 # Or you can play dirty (with Joker(s))
                 if card_type_in_hand != JOKER and hand[JOKER] > 0 \
@@ -58,27 +94,22 @@ def possible_next_moves(hand, board):
                         and hand[card_type_in_hand] + hand[JOKER] >= num_cards_in_board:
                     # Use one joker
                     if hand[card_type_in_hand] + 1 >= num_cards_in_board:
-                        joker_vec = get_cards_array(JOKER, 1)
-                        new_board = get_cards_array(
-                            card_type_in_hand, num_cards_in_board - 1) + joker_vec
-                        new_hand = hand - new_board
-                        possible_hands = np.vstack([possible_hands, new_hand])
-                        possible_boards = np.vstack(
-                            [possible_boards, new_board])
+                        possible_actions.append(
+                            get_cards_array(card_type_in_hand,
+                                            num_cards_in_board - 1)
+                            + get_cards_array(JOKER, 1)
+                        )
 
                     # Use two jokers
                     if hand[JOKER] == 2 and num_cards_in_board >= 3:
-                        joker_vec = get_cards_array(JOKER, 2)
-                        new_board = get_cards_array(
-                            card_type_in_hand, num_cards_in_board - 2) + joker_vec
-                        new_hand = hand - new_board
-                        possible_hands = np.vstack([possible_hands, new_hand])
-                        possible_boards = np.vstack(
-                            [possible_boards, new_board])
+                        possible_actions.append(
+                            get_cards_array(card_type_in_hand,
+                                            num_cards_in_board - 2)
+                            + get_cards_array(JOKER, 2)
+                        )
 
     # If board empty, passing not allowed
     if np.all(board == 0):
-        possible_hands = possible_hands[1:]
-        possible_boards = possible_boards[1:]
+        possible_actions = possible_actions[1:]
 
-    return possible_hands, possible_boards
+    return np.vstack(possible_actions)
--- egd/game/tests/moves_test.py
+++ egd/game/tests/moves_test.py
@@ -3,40 +3,32 @@ import pandas as pd
 import unittest
 
 from egd.game.cards import NUM_CARD_VALUES, AVAILABLE_CARDS
-from egd.game.moves import possible_next_moves
+from egd.game.moves import possible_next_moves, only_passing_possible
 
 
 class MovesTest(unittest.TestCase):
 
+    def test_only_passing_true(self):
+        """ Tests that only passing allowed. """
+
+        self.assertTrue(only_passing_possible(
+            np.zeros(NUM_CARD_VALUES), np.zeros(NUM_CARD_VALUES)
+        ))
+        self.assertTrue(only_passing_possible(
+            np.array([0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0]),
+            np.array([0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ))
+
     def test_possible_next_moves_empty_board(self):
         """ Tests possible moves on empty board. """
 
-        hand, board = possible_next_moves(
-            np.array([0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2]),
-            np.zeros(NUM_CARD_VALUES))
-        self.assertTrue(np.all(hand == np.array([
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 1],
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0],
-            [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1],
-            [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
-            [0, 2, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1],
-            [0, 2, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2],
-            [0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2],
-            [0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1],
-            [0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0]
-        ])))
-        self.assertTrue(np.all(board == np.array([
+        hand = np.array([0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2])
+        board = np.zeros(NUM_CARD_VALUES)
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
+
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == np.array([
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
@@ -62,57 +54,42 @@ class MovesTest(unittest.TestCase):
     def test_possible_next_moves_empty_board_lots_of_cards(self):
         """ Tests possible moves on empty board with lots of cards. """
 
-        hand, board = possible_next_moves(
-            AVAILABLE_CARDS,
-            np.zeros(NUM_CARD_VALUES))
+        hand = AVAILABLE_CARDS
+        board = np.zeros(NUM_CARD_VALUES)
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
 
-        true_hand = pd.read_csv(
-            "./egd/game/tests/test-data/hand.csv", header=None).values
-        true_board = pd.read_csv(
-            "./egd/game/tests/test-data/board.csv", header=None).values
+        true_actions = pd.read_csv(
+            "./egd/game/tests/test-data/actions.csv", header=None).values
 
-        self.assertTrue(np.all(hand == true_hand))
-        self.assertTrue(np.all(board == true_board))
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == true_actions))
 
     def test_possible_next_moves_non_empty_board(self):
         """ Tests possible next moves. """
 
-        hand, board = possible_next_moves(
-            np.array([0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2]),
-            np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0]))
-        self.assertTrue(np.all(hand == np.array([
-            [0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2],
-            [0, 2, 0, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1],
-            [0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2],
-            [0, 1, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 1]
-        ])))
-        self.assertTrue(np.all(board == np.array([
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0],
+        hand = np.array([0, 2, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 2])
+        board = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0])
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
+
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
             [0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
         ])))
 
-        hand, board = possible_next_moves(
-            np.array([1, 2, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 2]),
-            np.array([0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1]))
-        self.assertTrue(np.all(hand == np.array([
-            [1, 2, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 2],
-            [1, 2, 3, 1, 0, 0, 0, 0, 0, 4, 0, 0, 2],
-            [1, 2, 3, 1, 0, 0, 0, 1, 0, 4, 0, 0, 1],
-            [1, 2, 3, 1, 0, 0, 0, 2, 0, 4, 0, 0, 0],
-            [1, 2, 3, 0, 0, 0, 0, 3, 0, 4, 0, 0, 0],
-            [1, 2, 0, 1, 0, 0, 0, 3, 0, 4, 0, 0, 2],
-            [1, 2, 1, 1, 0, 0, 0, 3, 0, 4, 0, 0, 1],
-            [1, 2, 2, 1, 0, 0, 0, 3, 0, 4, 0, 0, 0],
-            [1, 0, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 1],
-            [1, 1, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 0],
-            [0, 2, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 0]
-        ])))
-        self.assertTrue(np.all(board == np.array([
-            [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1],
+        hand = np.array([1, 2, 3, 1, 0, 0, 0, 3, 0, 4, 0, 0, 2])
+        board = np.array([0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1])
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
+
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 1],
             [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 2],
@@ -128,25 +105,14 @@ class MovesTest(unittest.TestCase):
     def test_possible_next_moves_bug_jokers_only(self):
         """ Tests that a historic bug does not occur again. """
 
-        hand = np.array([
-            0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1
-        ], dtype=np.int8)
-        board = np.array([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0
-        ], dtype=np.int8)
-        next_hands, next_boards = possible_next_moves(
-            hand, board)
+        hand = np.array([0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1])
+        board = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
 
-        self.assertTrue(np.all(next_hands == np.array([
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 1, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 3, 2, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 1, 4, 2, 4, 1],
-            [0, 1, 0, 0, 0, 0, 0, 0, 2, 4, 2, 4, 1],
-            [0, 0, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1],
-        ])))
-        self.assertTrue(np.all(next_boards == np.array([
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
@@ -157,26 +123,14 @@ class MovesTest(unittest.TestCase):
     def test_possible_next_moves_bug_one_joker(self):
         """ Tests that a historic bug does not occur again. """
 
-        hand = np.array([
-            0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1
-        ], dtype=np.int8)
-        board = np.array([
-            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
-        ], dtype=np.int8)
-        next_hands, next_boards = possible_next_moves(
-            hand, board)
+        hand = np.array([0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1])
+        board = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+        actions = possible_next_moves(hand, board)
+        only_passing = only_passing_possible(hand, board)
 
-        self.assertTrue(np.all(next_hands == np.array([
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 2, 3, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 4, 1, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 2, 3, 2, 4, 1],
-            [0, 1, 0, 0, 0, 1, 0, 0, 1, 4, 2, 4, 1],
-            [0, 1, 0, 0, 0, 0, 0, 0, 2, 4, 2, 4, 1],
-            [0, 0, 0, 0, 0, 1, 0, 0, 2, 4, 2, 4, 1],
-        ])))
-        self.assertTrue(np.all(next_boards == np.array([
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        self.assertFalse(only_passing)
+        self.assertTrue(np.all(actions == np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
