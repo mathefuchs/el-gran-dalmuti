20d7f91688adfcb3fff65c4bf479e4d03fe80316
Tobias Fuchs <info@tobiasfuchs.de>
2020-12-26 22:34:58 +0100

    Increase number of validation games

--- egd/config.py
+++ egd/config.py
@@ -1,4 +1,4 @@
 # Config parameters for debug purposes
 use_small_nums = False
 epochs_for_validation = 10 if use_small_nums else 100
-validation_games = 2 if use_small_nums else 10
+validation_games = 2 if use_small_nums else 20
